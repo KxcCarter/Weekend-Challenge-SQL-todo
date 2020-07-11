ab957dc8742bf60f9ea74b35915597f781657623
Kenneth Carter <kxccarter@gmail.com>
2020-07-10 21:54:08 -0500

    Sets up GET route

--- routers/todo.router.js
+++ routers/todo.router.js
@@ -2,4 +2,29 @@ const express = require('express');
 const router = express.Router();
 const pool = require('../modules/pool');
 
+// GET
+
+router.get('/', (req, res) => {
+    console.log(`In GET route!`);
+
+    const query = `SELECT * FROM tasks;`;
+
+    pool
+        .query(query)
+        .then((dbRes) => {
+            console.log(`In dbRes`);
+            res.send(dbRes.rows);
+        })
+        .catch((err) => {
+            console.log(err);
+            res.sendStatus(500);
+        });
+});
+
+// POST
+
+// PUT
+
+// DELETE
+
 module.exports = router;
