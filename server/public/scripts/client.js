ab17055bcfb7c53fa46e237705735f3452f6ec0f
Kenneth Carter <kxccarter@gmail.com>
2020-07-10 22:26:17 -0500

    Adds GET ajax call in client.js

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -2,4 +2,37 @@ $(document).ready(init);
 
 function init() {
     console.log('js + jQ');
+
+    // on load render todo-list
+    getTasks();
+}
+
+function getTasks() {
+    // GETs data from database
+
+    $.ajax({
+        type: 'GET',
+        url: '/todo',
+    }).then((response) => {
+        console.log(response);
+        render(response);
+    });
+}
+
+function render(tasks) {
+    console.log(`in render`);
+    $('#js-list').empty();
+    for (let task of tasks) {
+        $('#js-list').append(`
+        <div class="card bg-light m-3 flex-column" style="max-width: 18rem;" data-id="${task.id}">
+            <div class="card-header">
+                <button class="btn btn-sm btn-primary">options</button>
+            </div>
+            <div class="card-body">
+              <h5 class="card-title js-task-title">${task.title}</h5>
+              <p class="card-text js-task-description">${task.description}</p>
+            </div>
+        </div>
+        `);
+    }
 }
